6e69a89b39d5473cd15df3307a047dba422a0dfe
Erick Merino Menares <erick.merino@mineduc.cl>
2022-08-04 14:29:32 -0400

    first auth testing

---
+++ restapi/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('check/', views.check, name='check'),
+    path('rbd/', views.rbd, name='rbd'),
+    path('rbds/', views.rbds, name='rbds'),
+    path('report/', views.report, name='report'),
+
+    path('signin', views.signin),
+    path('user_info/', views.user_info)
+
+]
--- restapi/views.py
+++ restapi/views.py
@@ -1,3 +1,74 @@
 from django.shortcuts import render
 
+from django.http import Http404, HttpResponse, JsonResponse
+from django.contrib.auth import authenticate
+
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
+import re
+from datetime import datetime, timedelta
+from functools import wraps
+
+from .authentication import token_expire_handler, expires_in
+from .serializers import UserSerializer, UserSigninSerializer
 # Create your views here.
+
+
+@api_view(["GET"])
+def login(request):
+    return HttpResponse("Hello, world.")
+
+
+@api_view(["GET"])
+def check(request):
+    return HttpResponse("Hello, world.")
+
+
+@api_view(["GET"])
+def rbd(request):
+    return HttpResponse("Hello, world.")
+
+
+@api_view(["GET"])
+def rbds(request):
+    return HttpResponse("Hello, world.")
+
+
+@api_view(["GET"])
+def report(request):
+    return HttpResponse("Hello, world.")
+
+
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def signin(request):
+    signin_serializer = UserSigninSerializer(data=request.data)
+    if not signin_serializer.is_valid():
+        return HttpResponse(signin_serializer.errors, status=400)
+
+    user = authenticate(
+        username=signin_serializer.data['username'],
+        password=signin_serializer.data['password']
+    )
+    # if not user:
+    #    return HttpResponse({'detail': 'Invalid Credentials or activate account'}, status=404)
+
+    # TOKEN STUFF
+    token, _ = Token.objects.get_or_create(user=user)
+
+    # token_expire_handler will check, if the token is expired it will generate new one
+    is_expired, token = token_expire_handler(token)  # The implementation will be described further
+    user_serialized = UserSerializer(user)
+
+    return JsonResponse({
+        'user': user_serialized.data,
+        'expires_in': expires_in(token),
+        'token': token.key
+    }, status=200)
+
+
+@api_view(["GET"])
+def user_info(request):
+    return HttpResponse("Hello, world.")
